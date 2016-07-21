1ebb41e7092fd4e29cd8565ef38f4980692c0e2d
gaelblanchard <gaelblanchard@live.com>
2016-07-20 23:23:01 -0400

    Update urls.py

--- goals/urls.py
+++ goals/urls.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #urls python
 from django.conf.urls import url
 from django.views.generic import TemplateView
@@ -15,7 +14,6 @@ urlpatterns = [
     url(r'^progress_report$', views.progressreport, name='progressreport'),
 ] 
 #.* syntax takes all possible strings
-=======
 from django.conf.urls import url
 from django.views.generic import TemplateView
 from django.conf import settings
@@ -31,5 +29,4 @@ urlpatterns = [
     url(r'^progress_report$', views.progressreport, name='progressreport'),
 ] 
 #.* syntax takes all possible strings
->>>>>>> 95b3be8256a9710e85d0df146cab5d786b693db7
 urlpatterns += staticfiles_urlpatterns()
